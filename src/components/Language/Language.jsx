c70fdc63ada430a729b8494ddd2a43237c7b1761
Svetlana <pokrowsckaya.sveta@gmail.com>
2022-03-22 13:01:08 +0300

    fix bug after reviw

--- src/components/Language/Language.jsx
+++ src/components/Language/Language.jsx
@@ -3,7 +3,7 @@ import { LangButton } from '../Buttons/Buttons'
 export const Language = ({ isVisible }) => {
   // думаю, здесь потом нужна будет логика для смены языка
   return (
-    <div className={isVisible ? 'languege open' : 'languege'}>
+    <div className={isVisible ? 'languege__open' : 'languege'}>
       <LangButton />
     </div>
   )
